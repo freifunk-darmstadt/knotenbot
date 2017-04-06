1b966b4129fc2b58cbe33884357717ede7b2fff5
blocktrron <blocktrron@users.noreply.github.com>
2017-04-06 10:12:38 +0200

    add nodeinfo command

--- modules/knotenbot.py
+++ modules/knotenbot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import re
 import sopel
 import requests
+from datetime import datetime, timedelta
 from sopel.config.types import StaticSection, ValidatedAttribute, ListAttribute
 from sopel.formatting import bold, color, colors
 knoten_url = 'https://map.darmstadt.freifunk.net/data/nodes.json'
@@ -20,7 +22,10 @@ def get_data(bot):
 
 
 def preprocess_data(nodes):
-    return dict((v['nodeinfo']['hostname'], dict(flags=v['flags'], network=v['nodeinfo']['network'], software=v['nodeinfo']['software']))
+    return dict((v['nodeinfo']['hostname'], dict(flags=v['flags'], network=v['nodeinfo']['network'],
+                                                 software=v['nodeinfo']['software'], statistics=v['statistics'],
+                                                 lastseen=v['lastseen'], hostname=v['nodeinfo']['hostname'],
+                                                 model=v['nodeinfo'].get('hardware', {}).get('model', 'N/A')))
                 for v in nodes)
 
 
@@ -74,6 +79,33 @@ def diff_status(data, old_data):
 
     return new, changed
 
+def find_node(bot, nodename):
+    if type(bot.memory['knoten']) is not dict or len(bot.memory['knoten'].keys()) is 0:
+        return None
+
+    if len(re.findall('^([0-9A-Fa-f]{2}([:-])?){5}([0-9A-Fa-f]{2})$', nodename)) is 1:
+        nodes = [bot.memory['knoten'][node] for node in bot.memory['knoten']
+                 if bot.memory['knoten'][node]['network']['mac'].replace(':', '').lower() == nodename.replace(':', '').lower()]
+        if len(nodes) is 1:
+            return nodes
+
+    return [bot.memory['knoten'][node] for node in bot.memory['knoten'] if nodename.lower() in node.lower()]
+
+def format_time(time):
+    time_difference = datetime.now() - time
+    minutes = time_difference.total_seconds() / 60
+    if minutes / 60 < 1:
+        return "{} minutes".format(int(round(minutes)))
+    if minutes / (60*24) < 1:
+        return "{} hours".format(int(round(minutes/(60))))
+    else:
+        return "{} days".format(int(round(minutes / (60*24))))
+
+def color_percentage(val):
+    if val >= 90:
+        return color(str(val), colors.RED)
+    else:
+        return color(str(val), colors.GREEN)
 
 @sopel.module.interval(30)
 def update_data(bot):
@@ -95,3 +127,48 @@ def update_data(bot):
         bot.msg('#ffda-log', ' ... and {} more addresses'.format(len(changed) - 10))
 
     bot.memory['knoten'] = data
+
+@sopel.module.commands('status')
+def status(bot, trigger):
+    search_queries = [n for n in trigger.args[1].split(' ')[1:] if len(n) > 0]
+    if len(search_queries) is 0:
+        bot.msg(trigger.sender, "Usage: .nodeinfo [nodename]")
+    for node in search_queries[:2]:
+        possible_nodes = find_node(bot, node)
+        if possible_nodes is None:
+            bot.msg(trigger.sender, "No Data yet")
+            break
+        elif len(possible_nodes) is 0:
+            bot.msg(trigger.sender, "No node with Name {}".format(node))
+        elif len(possible_nodes) is 1:
+            node = possible_nodes[0]
+            online = node['flags']['online']
+            time = datetime.strptime(node['lastseen'], '%Y-%m-%dT%H:%M:%S.%fZ')
+            nodename = bold(color(node['hostname'], colors.RED))
+            if online:
+                nodename = bold(color(node['hostname'], colors.GREEN))
+                time = datetime.now() - timedelta(seconds=node['statistics']['uptime'])
+
+            addr = node['network'].get('addresses', None)
+            if not addr:
+                addr = 'N/A'
+            else:
+                addr = "http://[{}]".format(sorted(addr)[0])
+            bot.msg(trigger.sender, "{}: {} - {} - {}({})".format(nodename,
+                                                                  format_time(time),
+                                                                  node['model'],
+                                                                  node['software']['firmware']['release'],
+                                                                  node['software']['firmware']['base']))
+            if online:
+                bot.msg(trigger.sender,
+                        "Load: {} - Memory: {} - Filesystem: {} - {}".format(
+                            color_percentage(int(round(node['statistics'].get('loadavg', 0) * 100))),
+                            color_percentage(round(node['statistics'].get('memory_usage', 0) * 100, 2)),
+                            color_percentage(round(node['statistics'].get('rootfs_usage', 0) * 100, 2)),
+                            addr))
+        elif len(possible_nodes) > 1:
+            max_full_hostnames = 3
+            msg_string = ", ".join(map(lambda x: x['hostname'], possible_nodes[:max_full_hostnames]))
+            if(len(possible_nodes) > max_full_hostnames):
+                msg_string = msg_string + " and {} more".format(len(possible_nodes)-max_full_hostnames)
+            bot.msg(trigger.sender, "More than one node containing '{}': {}".format(node, msg_string))
